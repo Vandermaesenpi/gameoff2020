5a95698b555d2c3090d24bb33e17f24631877a55
Pierre VANDERMAESEN <pvandermaesen@immersivefactory.com>
2020-11-03 22:39:31 +0100

    main interface prototype, the moon exist and can move

---
+++ Unity Project/Assets/SCRIPT/LevelEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class LevelEditor : MonoBehaviour
+{
+    public bool compute = false;
+    public bool computeStar = false;
+    public float radius;
+    public List<GameObject> buildingSpots;
+    public Transform starParent;
+    public GameObject starPrefab;
+    public int starNumber;
+    public float startDistance;
+    public Vector2 sizeRandom;
+    public Gradient starColor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Place building spots
+        if(compute){
+            float phi = Mathf.PI * (3f - Mathf.Sqrt(5f));
+
+            for (int i = 0; i < buildingSpots.Count; i++)
+            {
+                Vector3 newPos = Vector3.zero;
+                newPos.y = 1f - ((float)i/(float)(buildingSpots.Count -1)) * 2f;
+                float myRadius = Mathf.Sqrt(1f - newPos.y * newPos.y);
+                float theta = phi * (float)i;
+
+                newPos.x = Mathf.Cos(theta) * myRadius;
+                newPos.z = Mathf.Sin(theta) * myRadius;
+
+                buildingSpots[i].transform.localPosition = newPos * radius;
+                buildingSpots[i].transform.up = buildingSpots[i].transform.localPosition - Vector3.zero;
+            }
+        }
+
+        if(computeStar){
+            foreach (Transform child in starParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+            for (int i = 0; i < starNumber; i++)
+            {
+                GameObject newStar = Instantiate(starPrefab, starParent);
+                newStar.transform.position = Random.onUnitSphere * startDistance;
+                newStar.transform.localScale = Vector3.one * Random.Range(sizeRandom.x, sizeRandom.y);
+                newStar.GetComponent<Renderer>().material.color = starColor.Evaluate(Random.value);
+
+            }
+            computeStar = false;
+        }
+    }
+}
---
+++ Unity Project/Assets/SCRIPT/MoonRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotator : MonoBehaviour
+{
+    public bool canMove = true;
+    public Vector3 mousePos;
+    public float mouseSensitivity;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            Vector3 delta = Input.mousePosition - mousePos;
+            transform.RotateAround(Vector3.zero, Vector3.down,delta.x * mouseSensitivity);
+            transform.RotateAround(Vector3.zero, Vector3.right,delta.y * mouseSensitivity);
+        }
+        mousePos = Input.mousePosition;
+
+    }
+}
