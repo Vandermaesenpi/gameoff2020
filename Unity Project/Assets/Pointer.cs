2c0823d5290b193b86451632a54c15f929d53722
Pierre VANDERMAESEN <pvandermaesen@immersivefactory.com>
2020-11-04 01:47:36 +0100

    many many UI before going to bed

---
+++ Unity Project/Assets/Pointer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public Transform target;
+    public LineRenderer line;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null){
+            line.SetPosition(1,target.position);
+        }else{
+            line.SetPosition(1,line.GetPosition(0));
+        }
+    }
+}
